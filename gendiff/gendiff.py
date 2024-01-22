92b1329998a5b54963ffaaaf98d7c567849d04b7
Artemy Ablaev <artemyablaev.gmail.com>
2024-01-22 18:03:43 +0300

    step 2 done!

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -6,5 +6,6 @@ def gendiff():
 files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='see format of output')
     args = parser.parse_args()
     print(args)
