2dfc6ce2f60675163cee18010bfc779ebd7c1e4e
Artemy Ablaev <artemyablaev.gmail.com>
2024-01-24 17:03:30 +0300

    step 4 update. expected result in separate file

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -11,6 +11,6 @@ def fixtures_path():
 def test1_for_json(fixtures_path):
     first_file = os.path.join(fixtures_path, "file1.json")
     second_file = os.path.join(fixtures_path, "file2.json")
-    assert generate_diff(first_file, second_file) == ('{\n- \
-follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ \
-timeout: 20\n+ verbose: true\n}')
+    answer = os.path.join(fixtures_path, 'answer.py')
+    expected = open(answer, 'r')
+    assert generate_diff(first_file, second_file) == expected.read()
